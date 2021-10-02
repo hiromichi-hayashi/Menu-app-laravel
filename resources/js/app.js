d19065c6db67f746bfd3fc06f3002305ae330db6
廣内秀有 <shu.e va01-g.v@docomo.ne.jp>
2021-10-02 22:53:03 +0900

    ホーム画面のヘッダー、ハンバーガーメニューを追加

--- resources/js/app.js
+++ resources/js/app.js
@@ -20,8 +20,8 @@ window.Vue = require("vue").default;
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component(
-    "example-component",
-    require("./components/ExampleComponent.vue").default
+    "app",
+    require("./components/App.vue").default
 );
 
 /**
