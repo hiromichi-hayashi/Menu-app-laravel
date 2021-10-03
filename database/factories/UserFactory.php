58c73c69191ce421e63836eb2da2fdae1700db32
hh-pd <hiromiti999@gmail.com>
2021-10-03 17:40:06 +0900

    [fix]セッション修正

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,7 +26,7 @@ class UserFactory extends Factory
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => $this->faker->password(), 
+            'password' => bcrypt('abcdefg'), 
             'remember_token' => Str::random(10),
         ];
     }
