2a5c4f00226fea6b3da91a6e2f462c498ca32f43
hh-pd <hiromiti999@gmail.com>
2021-10-07 00:45:45 +0900

    [feature]ログイン画面、サインイン画面の作製

---
+++ resources/js/components/Login.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="app">
+		<Layout> </Layout>
+	</div>
+</template>
+
+<script>
+import Layout from "./layout/layout.vue";
+
+export default {
+	name: "App",
+	components: {
+		Layout,
+	},
+};
+</script>
+<style lang="scss"></style>
